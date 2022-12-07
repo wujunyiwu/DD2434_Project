302d87fb004e76d1a284f78ebd995bf57de2d731
JunyiWu-CN <15816170054@163.com>
2022-12-07 23:01:41 +0100

    add dataset MNIST

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.utilities.seed import seed_everything
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
-from dataset import VAEDataset
+from dataset import VAEDataset, MyDataset
 from pytorch_lightning.plugins import DDPPlugin
 
 
@@ -39,7 +39,11 @@ model = vae_models[config['model_params']['name']](**config['model_params'])
 experiment = VAEXperiment(model,
                           config['exp_params'])
 
-data = VAEDataset(**config["data_params"], pin_memory=len(config['trainer_params']['gpus']) != 0)
+if config["data_set"]["name"] == "celeba":
+    data = VAEDataset(**config["data_params_celeba"], pin_memory=len(config['trainer_params']['gpus']) != 0)
+elif config["data_set"]["name"] == "MNIST":
+    data = MyDataset(**config["data_params_MNIST"], pin_memory=len(config['trainer_params']['gpus']) != 0)
+
 
 data.setup()
 runner = Trainer(logger=tb_logger,
